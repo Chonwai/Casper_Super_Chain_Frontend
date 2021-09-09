75c771f7f7b04ea1727a2c1a3627ef51f0d8662c
Chonwai <chonwaiun@gmail.com>
2021-09-09 15:11:39 +0800

    Add: Added the about us page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Footer from '../components/layouts/Footer';
+import Header from '../components/layouts/Header';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>Casper Super Chain</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
             <Component {...pageProps} />
             <Footer />
         </>
---
+++ pages/about/AboutContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import AboutView from './AboutView';
+
+export default function AboutContainer() {
+    return <AboutView />;
+}

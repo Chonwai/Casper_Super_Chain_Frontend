aa71f77c40c4531d2389bfb551032437ee37f81c
Chonwai <chonwaiun@gmail.com>
2021-09-09 13:47:14 +0800

    Add: Added the cypress and some necessary packages.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        'cypress/globals': true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'plugin:cypress/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'cypress'],
+    rules: {
+        'cypress/no-assigning-return-values': 0,
+        'cypress/no-unnecessary-waiting': 0,
+        'cypress/assertion-before-screenshot': 0,
+        'cypress/no-force': 0,
+        'cypress/no-async-tests': 0,
+        'react/jsx-filename-extension': [
+            0,
+            {
+                allow: 'as-needed',
+            },
+        ],
+        'react/jsx-props-no-spreading': [
+            0,
+            {
+                html: 'ignore' || 'enforce',
+                custom: 'ignore' || 'enforce',
+                explicitSpread: 'ignore' || 'enforce',
+                exceptions: 'ignore' || 'enforce',
+            },
+        ],
+        'import/no-extraneous-dependencies': ['error', { peerDependencies: true }],
+    },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.elementType.isRequired,
+};
+
+export default MyApp;
